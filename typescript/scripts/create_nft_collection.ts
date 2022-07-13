3d713b62d0a5cee396ca6bf8e4664d5f8ae2898e
Blas Cerecer <blas@staratlas.com>
2022-07-12 22:48:55 -0700

    bug: works but needs new @martiandao/aptos-web3.js package

--- typescript/scripts/create_nft_collection.ts
+++ typescript/scripts/create_nft_collection.ts
@@ -1,49 +1,49 @@
 // Copyright (c) The Aptos Foundation
 // SPDX-License-Identifier: Apache-2.0
-const prompt = require("prompt");
-
+import { WalletClient, FaucetClient } from "@martiandao/aptos-web3.js";
 import {
   Account,
   RestClient,
   TESTNET_URL,
   FAUCET_URL,
-  FaucetClient,
 } from "../first_transaction";
 import { TokenClient } from "../first_nft";
 
 const maxFaucetAmount = 20_000;
-const restClient = new RestClient(TESTNET_URL);
-const faucetClient = new FaucetClient(FAUCET_URL, restClient);
+const faucetClient = new FaucetClient(TESTNET_URL, FAUCET_URL);
+const walletClient = new WalletClient(TESTNET_URL, FAUCET_URL);
 
 async function main() {
-  const tokenClient = new TokenClient(restClient);
-
-  const collection_creator = new Account();
+  // create new wallet (10 test tokens airdropped by default)
+  console.log("\n=== Wallet Creation ===");
+  const collectionCreatorWallet = await walletClient.createWallet();
+  const collectionCreatorAddress = collectionCreatorWallet.accounts[0].address;
+  const signingKey = collectionCreatorWallet.code;
+  const collectionCreatorAccount = await walletClient.getAccountFromMetaData(
+    signingKey,
+    collectionCreatorWallet.accounts[0]
+  );
   const collection_name = "Test Collection";
   const token_name = "Test token";
 
   console.log("\n=== Addresses ===");
   console.log(
-    `Collection creator Account: 0x${collection_creator.address()} SecretKey: ${Buffer.from(
-      collection_creator.signingKey.secretKey
-    )
-      .toString("hex")
-      .slice(0, 64)}`
+    `Collection creator Account: 0x${collectionCreatorAddress} SecretKey: ${signingKey}`
   );
 
-  await faucetClient.fundAccount(collection_creator.address(), maxFaucetAmount);
+  await faucetClient.fundAccount(collectionCreatorAddress, maxFaucetAmount);
 
   console.log("\n=== Initial Balances ===");
   console.log(
-    `Collection creator: ${await restClient.accountBalance(
-      collection_creator.address()
+    `Collection creator: ${await walletClient.getBalance(
+      collectionCreatorAddress
     )}`
   );
 
   console.log("\n=== Creating Collection and Token ===");
 
-  await tokenClient.createCollection(
-    collection_creator,
+  await walletClient.createCollection(
+    collectionCreatorAccount,
     collection_name,
     "Collection creator's simple collection",
     "https://aptos.dev"
@@ -52,14 +52,11 @@ async function main() {
   for (let i = 1; i <= 5; i++) {
     // Transfer maxFaucetAmount for every modded variable to avoid going out of balance
     if (i % 10 == 0) {
-      await faucetClient.fundAccount(
-        collection_creator.address(),
-        maxFaucetAmount
-      );
+      await faucetClient.fundAccount(collectionCreatorAddress, maxFaucetAmount);
     }
 
-    await tokenClient.createToken(
-      collection_creator,
+    await walletClient.createToken(
+      collectionCreatorAccount,
       collection_name,
       `${token_name} ${i}`,
       "Collection creator's any description",
@@ -67,6 +64,12 @@ async function main() {
       "https://aptos.dev/img/nyan.jpeg"
     );
   }
+
+  console.log(
+    `Collection creator: ${JSON.stringify(
+      await walletClient.getTokens(collectionCreatorAddress)
+    )}`
+  );
 }
 
 if (require.main === module) {

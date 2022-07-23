bb11c84ff289be23c997f7a3120e72a92b1c47eb
Blas Cerecer <blas@staratlas.com>
2022-07-22 18:31:26 -0700

    feat: add create_nyan_nft_collection script

---
+++ typescript/scripts/nyan/nyanCollectionData.ts
@@ -0,0 +1,105 @@
+export const collectionData = {
+  name: "Aptos Nyan Cats",
+  description: "The most nyantic collection in the Aptosverse",
+  uri: "https://spacepowder.io",
+};
+const bucketuri =
+  "https://rxbadlmhqshszwaxifut.supabase.co/storage/v1/object/public/aptos-nyan-cats/";
+export const tokenData = [
+  {
+    name: "Nyan Cat",
+    uri: `${bucketuri}nyan_cat.gif`,
+  },
+  {
+    name: "Angelic Nyan Cat",
+    uri: `${bucketuri}angelic_nyan.gif`,
+  },
+  {
+    name: "Demonic Nyan Cat",
+    uri: `${bucketuri}demonic_nyan.gif`,
+  },
+  {
+    name: `New Year's Nyan Cat`,
+    uri: `${bucketuri}new_year_nyan.gif`,
+  },
+  {
+    name: "Pirate Nyan Cat",
+    uri: `${bucketuri}pirate_nyan_cat.gif`,
+  },
+  {
+    name: "Lucky Nyan Cat",
+    uri: `${bucketuri}lucky_nyan.gif`,
+  },
+  {
+    name: "Celebracion Nyan Cat",
+    uri: `${bucketuri}celebracion_nyan.gif`,
+  },
+  {
+    name: "Nyantendo Nyan Cat",
+    uri: `${bucketuri}nyantendo.gif`,
+  },
+  {
+    name: "Holographic Nyan Aoki",
+    uri: `${bucketuri}holographic_nyan_aoki.gif`,
+  },
+  {
+    name: "Biker Nyan Cat",
+    uri: `${bucketuri}biker_nyan.gif`,
+  },
+  {
+    name: "Nyan Snoop Dogg",
+    uri: `${bucketuri}nyan_snoop_dogg.gif`,
+  },
+  {
+    name: "Hazy Nyan Cat",
+    uri: `${bucketuri}hazy_nyan.gif`,
+  },
+  {
+    name: "Nyan Aoki",
+    uri: `${bucketuri}nyan_aoki.gif`,
+  },
+  {
+    name: "Shiny Nyan Cat",
+    uri: `${bucketuri}shiny_nyan.gif`,
+  },
+  {
+    name: "Fancy Nyan Cat",
+    uri: `${bucketuri}fancy_nyan.gif`,
+  },
+  {
+    name: "Star-Spangled Nyan Cat",
+    uri: `${bucketuri}star_spangled_nyan.gif`,
+  },
+  {
+    name: "Fiesta Dog",
+    uri: `${bucketuri}fiest_dog.gif`,
+  },
+  {
+    name: "Nyan Dogecoin",
+    uri: `${bucketuri}nyan_dogecoin.gif`,
+  },
+  {
+    name: "Surfing Nyan Cat",
+    uri: `${bucketuri}surfing_nyan.gif`,
+  },
+  {
+    name: "Glitch Nyan Cat",
+    uri: `${bucketuri}glitch_nyan.gif`,
+  },
+  {
+    name: "Nyan Doodle",
+    uri: `${bucketuri}nyan_doodle.png`,
+  },
+  {
+    name: "Terra Nyan Cat",
+    uri: `${bucketuri}terra_nyan.gif`,
+  },
+  {
+    name: "Golden Nyan Cat",
+    uri: `${bucketuri}golden_nyan.gif`,
+  },
+  {
+    name: "Vaporwave Nyan Cat",
+    uri: `${bucketuri}vaporwave_nyan.gif`,
+  },
+];
